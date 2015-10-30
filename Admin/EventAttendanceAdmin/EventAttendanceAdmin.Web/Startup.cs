58aaf560be4756a56537f864fe360bdf76b4bb1e
Scott Metoyer <scott.metoyer@gmail.com>
2015-10-29 19:37:58 -0700

    Added mobile and web apps

---
+++ Admin/EventAttendanceAdmin/EventAttendanceAdmin.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventAttendanceAdmin.Web.Startup))]
+namespace EventAttendanceAdmin.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
