aaba778f785cc2e9df00371136cb4861450e3b89
Scott Metoyer <scott.metoyer@gmail.com>
2015-10-30 19:40:40 -0700

    Created entities

---
+++ Admin/EventAttendanceAdmin/EventAttendanceAdmin.Web/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventAttendanceAdmin.Web.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+
+        public string Name { get; set; }
+
+        public byte[] QRCode { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
---
+++ Admin/EventAttendanceAdmin/EventAttendanceAdmin.Web/Models/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventAttendanceAdmin.Web.Models
+{
+    public class Registration
+    {
+        public int RegistrationId { get; set; }
+
+        public int EventId { get; set; }
+  
+        public string StudentIdentifier { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
