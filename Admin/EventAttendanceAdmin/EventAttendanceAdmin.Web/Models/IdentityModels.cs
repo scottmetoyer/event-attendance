772f06ab02e1c79e3e49fcab828ceea7eb373b13
Scott Metoyer <scott.metoyer@gmail.com>
2015-11-04 12:58:00 -0800

    Switched EF authentication to SQL Server
    Code first is truly awesome.

--- Admin/EventAttendanceAdmin/EventAttendanceAdmin.Web/Models/IdentityModels.cs
+++ Admin/EventAttendanceAdmin/EventAttendanceAdmin.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace EventAttendanceAdmin.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EventContext", throwIfV1Schema: false)
         {
         }
 
