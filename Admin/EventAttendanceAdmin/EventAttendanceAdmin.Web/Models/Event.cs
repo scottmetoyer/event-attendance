f877eecc06471ac15c8963895256097126269dff
Scott Metoyer <scott.metoyer@gmail.com>
2015-11-03 20:29:11 -0800

    Added location property to Event model

--- Admin/EventAttendanceAdmin/EventAttendanceAdmin.Web/Models/Event.cs
+++ Admin/EventAttendanceAdmin/EventAttendanceAdmin.Web/Models/Event.cs
@@ -12,6 +12,8 @@ namespace EventAttendanceAdmin.Web.Models
 
         public string Name { get; set; }
 
+        public string Location { get; set; }
+
         public DateTime Start { get; set; }
 
         public DateTime End { get; set; }
