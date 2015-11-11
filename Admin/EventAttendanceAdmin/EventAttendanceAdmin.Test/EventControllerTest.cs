b5fd54e7a2816ab652a5611b5cb546c0f72f3d35
Scott Metoyer <scott.metoyer@gmail.com>
2015-11-10 20:09:11 -0800

    Started implementing project unit tests

---
+++ Admin/EventAttendanceAdmin/EventAttendanceAdmin.Test/EventControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventAttendanceAdmin.Web.Controllers;
+using System.Web.Mvc;
+using EventAttendanceAdmin.Web.Models;
+
+namespace EventAttendanceAdmin.Test
+{
+    [TestClass]
+    public class EventControllerTest
+    {
+        [TestMethod]
+        public void TestIndexView()
+        {
+        }
+
+        public void TestDetailView()
+        {
+        }
+
+        public void TestCreateView() { }
+
+        public void TestEditView()
+        {
+
+        }
+
+        public void TestDeleteView() { }
+
+        [TestMethod]
+        public void TestPrintView()
+        {
+            var controller = new EventController();
+            var result = controller.Print(2) as ViewResult;
+            var evt = (Event)result.ViewData.Model;
+            Assert.AreEqual("Zumba", evt.Name);
+        }
+    }
+}
